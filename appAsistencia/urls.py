e936c5eb9c53dd8e681ac20da68f657f165fd97b
whoisml <medinala96@hotmail.com>
2019-02-10 18:21:51 -0600

    Vistas y plantillas para detalles de usuarios

--- appAsistencia/urls.py
+++ appAsistencia/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-	path('', views.lista_asistencia, name='lista_asistencia')
+	path('', views.lista_asistencia, name='lista_asistencia'),
+	path('usuario/<str:pk>/', views.detalles_usuario, name='detalles_usuario')
 ]
--- appAsistencia/views.py
+++ appAsistencia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Usuario, Asistencia
 from django.utils import timezone
 
@@ -7,3 +7,7 @@ def lista_asistencia(request):
 	#lista_asistencia = Asistencia.objects.filter(hora_entrada=timezone.now()).order_by('hora_entrada')
 	lista_usuarios = Usuario.objects.all()
 	return render(request, 'appAsistencia/lista_asistencia.html', {'lista_usuarios':lista_usuarios})
+
+def detalles_usuario(request, pk):
+	detalles=get_object_or_404(Usuario, pk=pk)
+	return render(request, 'appAsistencia/detalles_usuario.html', {'detalles':detalles})
